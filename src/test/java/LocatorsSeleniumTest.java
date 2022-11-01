b9230a11c046e3825821edde5114d78888d3686f
Diana <d.klavdeva@gmail.com>
2022-11-01 20:08:17 +0200

    HW / Locators

---
+++ src/test/java/LocatorsSeleniumTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+
+public class LocatorsSeleniumTest {
+
+    .//a[@href = 'https://www.discovercars.com']
+            .//a[@id = 'currentCurrency']
+            .//div[@class = 'home-caption']
+            .//div[@class = 'fake-field clearfix']
+            .//span[@class = 'sb-residence-text']\
+            .//button[@id = 'location-submit']
+            .//div[@id = 'review-arrow-left']
+            .//div[@class = 'tp-widget-review']
+            .//div[@class = 'row is-table-row']
+            .//div[@data-id = 'car-groups-small']
+            .//div[@class = 'car-top-filter-control next']
+            .//span[@class = 'showing-cars']
+            .//a[@class = 'dc-ui dropdown-toggle text-blue-500']
+            .//div [@class = 'flt-type']
+            .//span [@class = 'min-price']
+            .//label [@for = 'fuel-type-ft-V']
+            .//label [@for = 'number-of-seats-seats-4']
+            .//a [@class= 'open-modal search-index trigger-gtm-rc rental-conditions with-logo']
+            .//a [@class= 'btn btn-book submit click-on-car selected-car prevent-loading instant']
+            .//div [@class= 'dc-ui badges mt-12']
+            .//div [@class= 'supplier-rating']
+            .//div [@class= 'price-item-price-main']
+}
