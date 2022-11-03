9652848f087ea21fee49e9993112ad8165f80256
Diana <d.klavdeva@gmail.com>
2022-11-03 20:43:54 +0200

    HW / Locators

--- src/test/java/LocatorsSeleniumTest.java
+++ src/test/java/LocatorsSeleniumTest.java
@@ -1,32 +1,32 @@
 import org.openqa.selenium.By;
 
 public class LocatorsSeleniumTest {
-        .//a[@href = 'https://www.discovercars.com'];
-        .//a[@id = 'currentCurrency'];
-        .//a[@data-label = 'Navigation'];
-        .//div[@class = 'home-caption'];
-        .//div[@class = 'fake-field clearfix'];
-        .//u[@id = 'sb-country'];
-        .//button[@id = 'location-submit'];
-        .//div[@id = 'review-arrow-left'];
-        .//div[@class = 'tp-widget-review'];
-        .//div[@class = 'row is-table-row'];
-        .//div [@class = 'flt-type'];
-        .//span[@class = 'min-price'];
-        .//label[@for = 'fuel-type-ft-V'];
-        .//label[@for = 'number-of-seats-seats-4'];
-        .//div[@data-id = 'car-groups-small'];
-        .//div[@class = 'car-top-filter-control next'];
-        .//span[@class = 'showing-cars'];
-        .//div[@class = 'dc-ui dropdown inline-block'];
-        .//ul[@class = 'dc-list dc-list-icon dc-list-md text-14 dc-list-horizontal dc-list-mt-8 car-params mt-8 text-gray-500'];
-        .//a[@class = 'open-modal search-index trigger-gtm-rc rental-conditions with-logo'];
-        .//a[@class = 'btn btn-book submit click-on-car selected-car prevent-loading instant'];
-        .//div[@class = 'dc-ui badges mt-12'];
-        .//div[@class = 'supplier-rating'];
-        .//div[@class= 'price-item-price-main']
 
-                .//div[@class = 'spinner-container'];
-                .//div[@class = 'mb-8 text-24 text-bold text-center'];
-                .//div[@class = 'mb-24 text-center'];
+    private final By LOGO_CELL = By.className("logo-cell");
+    private final By NAVIGATION_CURRENCY = By.id("currentCurrency");
+    private final By NAVIGATION_LOG_IN = By.className("navigation-link action my-booking trigger-login-modal pointer");
+    private final By HOME_CAPTION = By.className("home-caption");
+    private final By PICK_DATE_LEFT = By.className("fake-field clearfix");
+    private final By RESIDENCE_COUNTRY = By.id("sb-country");
+    private final By SEARCH_BTN = By.id("location-submit");
+    private final By REVIEW_ARROW_LEFT = By.id("review-arrow-left");
+    private final By WIDGET_REVIEW = By.className("tp-widget-review");
+    private final By TABLE_ROW = By.className("row is-table-row");
+    private final By CHECKBOX_SPECIAL_OFFERS = By.className("flt-type");
+    private final By MIN_PRICE = By.className("min-price");
+    private final By FUEL_TYPE = By.xpath(".//label[@for = 'fuel-type-ft-V']");
+    private final By NUMBER_OF_SEATS = By.xpath(".//label[@for = 'number-of-seats-seats-4']");
+    private final By CAR_TOP_FILTER = By.className("car-top-filter-item car-groups-item filtering-event init");
+    private final By CAR_TOP_FILTER_CONTROL_NEXT = By.className("car-top-filter-control next");
+    private final By SHOWING_CARS = By.className("showing-cars");
+    private final By DROPDOWN_INLINE_BLOCK = By.className("dc-ui dropdown inline-block");
+    private final By PARAMS_LIST = By.className("dc-list dc-list-icon dc-list-md text-14 dc-list-horizontal dc-list-mt-8 car-params mt-8 text-gray-500");
+    private final By VIEW_BTN = By.className("btn btn-book submit click-on-car selected-car prevent-loading instant");
+    private final By RENTAL_CONDITIONS_LINK = By.className("open-modal search-index trigger-gtm-rc rental-conditions with-logo");
+    private final By BADGES_DESC = By.className("dc-ui badges mt-12");
+    private final By SUPPLIER_RATING = By.className("supplier-rating");
+    private final By PRICE_MAIN = By.className("price-item-price-main");
+    private final By SEARCH_SPINNER = By.className("spinner-container");
+    private final By BOLD_TEXT_CENTER = By.className("mb-8 text-24 text-bold text-center");
+    private final By TEXT_CENTER = By.className("mb-24 text-center");
 }
