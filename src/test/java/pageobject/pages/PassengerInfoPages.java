5c360fc6c3f521a28d96ab202cd030346194af91
Diana <d.klavdeva@gmail.com>
2022-12-08 22:07:41 +0200

    Base Functions / Home page / Passenger Info page

---
+++ src/test/java/pageobject/pages/PassengerInfoPages.java
@@ -0,0 +1,11 @@
+package pageobject.pages;
+
+import pageobject.BaseFunc;
+
+public class PassengerInfoPages {
+    private BaseFunc baseFunc;
+
+    public PassengerInfoPages(BaseFunc baseFunc) {
+        this.baseFunc = baseFunc;
+    }
+}
