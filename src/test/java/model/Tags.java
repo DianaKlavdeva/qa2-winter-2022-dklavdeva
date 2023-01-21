544d2e8739a0b55412b324272a0b4e170bccaa5c
Diana <d.klavdeva@gmail.com>
2023-01-21 12:42:28 +0200

    HW / Creating objects model based on JSON

---
+++ src/test/java/model/Tags.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Tags {
+    private String tags;
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+}
